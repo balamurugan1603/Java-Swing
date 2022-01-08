49eb3a7760bc458feb963204dac253cff4389afe
Balamurugan P <balacse1622003@gmail.com>
2022-01-08 14:28:08 +0530

    Added Hashtable, Stack

--- Collections/Collections.java
+++ Collections/Collections.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
 import java.util.Vector;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Stack;
 import java.util.Iterator;
 
 class Collections {
@@ -21,6 +24,26 @@ class Collections {
         Iterator v_iter = v.iterator();
         while(v_iter.hasNext()) {
             System.out.println(v_iter.next());
-        } 
+        }
+
+        // Hashtable
+        Hashtable<Integer, String> ht = new Hashtable<Integer, String>();
+        ht.put(1, "name1");    // ht.get(1);
+        ht.put(2, "name2");    // ht.get(2);
+        ht.put(3, "name3");    // ht.get(3);
+        System.out.println(ht.entrySet());
+        for(Map.Entry element: ht.entrySet()) {
+            System.out.println("Key: " + element.getKey() + ", Value: " + element.getValue());
+        }
+
+        // Stack - FILO
+        Stack<String> st = new Stack<String>();
+        st.push("name1");
+        st.push("name2");
+        st.push("name3");
+        while(!st.empty()){
+            // st.peek() => Return stack top without deleting
+            System.out.println(st.pop());
+        }
     }
 }
